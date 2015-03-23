801ff3b1dec9a478b849db78fd0ea1bfe7bbb38c
Chathuri Gunawardhana <lanch.gunawardhana@gmail.com>
2015-03-23 11:48:28 +0000

    added a comment

--- LibPADIMapNoReduce/IWorkerTracker.cs
+++ LibPADIMapNoReduce/IWorkerTracker.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 namespace PADIMapNoReduce
 {
+    /*As we habve to marshall worker object(we don't have different url for job tracker
+     * ) activator should also be the same type*/
    public interface IWorkerTracker:IWorker,IJobTracker
     {
     }
