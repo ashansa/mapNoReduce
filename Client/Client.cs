e7c709f09b21319ee9bccbfdfdd4d8e8a132ec91
ashansa <ashansa.perera@gmail.com>
2015-04-03 20:47:19 +0100

    minor refactoring

--- Client/Client.cs
+++ Client/Client.cs
@@ -170,7 +170,6 @@ namespace PADIMapNoReduce
             byte[] target = new byte[size+additional];
             fs.Read(target, 0, (int)(endByte+additional  - startByte));
             fs.Close();
-            File.WriteAllBytes(outputDir + Path.DirectorySeparatorChar + "test.txt", target);
             //string split = new UnicodeEncoding().GetString(target);
             string split = System.Text.Encoding.UTF8.GetString(target);
             split = split.Trim();
